7ac70ad14f5eab8f39346b1924bd861bf9ad6594
ClausNMadsen <claus.normann@hotmail.com>
2024-01-14 23:42:52 +0100

    Fixed a few bugs and solved part 2! Shoelace and Pick's for the win!

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -4,7 +4,35 @@ fn main() {
     let text =
         std::fs::read_to_string("data/10a.txt").expect("Couldn't read file at hard-coded path!");
     println!("Part one result: {:?}", part1(&text));
-    //     println!("Part two result: {:?}", part2(&text));
+    println!("Part two result: {:?}", part2(&text));
+}
+
+#[allow(clippy::cast_precision_loss)]
+fn part2(block: &str) -> f64 {
+    let vecblock = read_block(block);
+    let start = get_start(&vecblock);
+    let mut dir = find_first_direction(&vecblock, &start);
+    let mut locs_in_loop: Vec<(f64, f64)> = vec![];
+    let (mut cur_pip, mut loc) = go_to_next_pipe(&vecblock, &start, dir);
+    locs_in_loop.push((loc.0 as f64, loc.1 as f64));
+    while cur_pip != Pipe::S {
+        dir = cur_pip.traverse(dir.into()).unwrap();
+        (cur_pip, loc) = go_to_next_pipe(&vecblock, &loc, dir);
+        locs_in_loop.push((loc.0 as f64, loc.1 as f64));
+    }
+
+    // Shoelace algorithm
+    let mut twice_area: f64 = locs_in_loop
+        .windows(2)
+        .map(|tups| tups[0].0.mul_add(tups[1].1, -(tups[0].1 * tups[1].0)))
+        .sum();
+    twice_area += {
+        let first = locs_in_loop.first().unwrap();
+        let last = locs_in_loop.last().unwrap();
+        last.0.mul_add(first.1, -(last.1 * first.0))
+    };
+    // Picks algorithm
+    (twice_area.abs() + 2.0 - ((locs_in_loop.len()) as f64)) / 2.0
 }
 
 fn part1(block: &str) -> i64 {
@@ -12,10 +40,10 @@ fn part1(block: &str) -> i64 {
     let start = get_start(&vecblock);
     let mut counter: i64 = 1;
     let mut dir = find_first_direction(&vecblock, &start);
-    let (mut cur_pip, mut loc) = go_to_next_pipe(&vecblock, &start, &dir);
+    let (mut cur_pip, mut loc) = go_to_next_pipe(&vecblock, &start, dir);
     while cur_pip != Pipe::S {
         dir = cur_pip.traverse(dir.into()).unwrap();
-        (cur_pip, loc) = go_to_next_pipe(&vecblock, &loc, &dir);
+        (cur_pip, loc) = go_to_next_pipe(&vecblock, &loc, dir);
         counter += 1;
     }
     counter.div(2)
@@ -37,7 +65,7 @@ fn get_start(block: &[Vec<char>]) -> (usize, usize) {
 fn go_to_next_pipe(
     block: &[Vec<char>],
     start: &(usize, usize),
-    out: &Out,
+    out: Out,
 ) -> (Pipe, (usize, usize)) {
     let loc = out.step(start);
     let c = block[loc.0][loc.1];
@@ -46,28 +74,28 @@ fn go_to_next_pipe(
 
 fn find_first_direction(block: &[Vec<char>], start: &(usize, usize)) -> Out {
     for direction in [
-        Out(Direction::North),
         Out(Direction::East),
         Out(Direction::South),
+        Out(Direction::North),
         Out(Direction::West),
     ] {
         let loc = direction.step(start);
         let pipe = Pipe::from_char(block[loc.0][loc.1]);
         if let Some(p) = pipe {
             let out = p.traverse(direction.into());
-            if let Some(a) = out {
-                return a;
+            if out.is_some() {
+                return direction;
             }
         }
     }
     panic!("There should always be a valid first direction!")
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Out(Direction);
 
 impl Out {
-    const fn step(&self, start: &(usize, usize)) -> (usize, usize) {
+    const fn step(self, start: &(usize, usize)) -> (usize, usize) {
         match self {
             Self(Direction::North) => (start.0 - 1, start.1),
             Self(Direction::South) => (start.0 + 1, start.1),
@@ -91,7 +119,7 @@ impl From<Out> for In {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     North,
     South,
@@ -99,7 +127,7 @@ enum Direction {
     East,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 enum Pipe {
     Bar,
     Dash,
@@ -124,7 +152,7 @@ impl Pipe {
         }
     }
 
-    const fn traverse(&self, direction: In) -> Option<Out> {
+    const fn traverse(self, direction: In) -> Option<Out> {
         match (self, direction) {
             (Self::Bar, In(Direction::West | Direction::East))
             | (Self::Dash, In(Direction::North | Direction::South))
@@ -166,10 +194,53 @@ SJLL7
 |F--J
 LJ.LJ";
 
+    const INPUT3: &str = "\
+    ...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+
+    const INPUT4: &str = "\
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+
+    const INPUT5: &str = "\
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
     #[test]
     fn day10_part1() {
         assert_eq!(get_start(&read_block(INPUT1)), (1, 1));
         assert_eq!(get_start(&read_block(INPUT2)), (2, 0));
         assert_eq!(part1(INPUT1), 4);
+    }
+
+    #[test]
+    fn day10_part2() {
+        assert_eq!(part2(INPUT1), 1.0);
+        assert_eq!(part2(INPUT2), 1.0);
+        assert_eq!(part2(INPUT3), 4.0);
+        assert_eq!(part2(INPUT4), 8.0);
+        assert_eq!(part2(INPUT5), 10.0);
     }
 }
